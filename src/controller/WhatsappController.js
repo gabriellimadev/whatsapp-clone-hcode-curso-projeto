fa4b1b4c8a69571a0c4a071c322287a449a4965a
gabriellimadev <gslima.dts@gmail.com>
2021-07-06 18:12:53 -0300

    W07 - Events on 'menuAttach'

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -163,28 +163,80 @@ class WhatsAppController{
         });
 
         this.el.btnAttachPhoto.on('click', e=>{
-            console.log('click')
+            this.el.inputPhoto.click()
+        })
+
+        this.el.inputPhoto.on('change', e=>{
+
+            console.log(this.el.inputPhoto.files);
+            
+            [...this.el.inputPhoto.files].forEach(file=>{
+
+                console.log(file)
+            })
         })
 
         this.el.btnAttachCamera.on('click', e=>{
-            console.log('click')
-        })
+
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                height: '100%'
+            });
+        });
+
+        this.el.btnClosePanelCamera.on('click', e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e=>{
+            console.log('take picture');
+        });
 
         this.el.btnAttachDocument.on('click', e=>{
-            console.log('click')
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                height: '100%'
+            })
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+
+        this.el.btnSendDocument.on('click', e=>{
+
+            console.log('sendd')
         })
 
         this.el.btnAttachContact.on('click', e=>{
-            console.log('click')
-        })
+            
+            this.el.modalContacts.show();
+        });
 
+        this.el.btnCloseModalContacts.on('click', e=>{
+
+            this.el.modalContacts.hide();
+        })
+    }
+
+    closeAllMainPanel(){
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
     }
 
     closeMenuAttach(e){
          
         document.removeEventListener('click', this.closeMenuAttach);
         this.el.menuAttach.removeClass('open');
-        console.log('remove menu')
     }
 
     closeAllLeftPanels(){
